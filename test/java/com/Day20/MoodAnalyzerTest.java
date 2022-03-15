061cd8f01b93bfddaf55717bead6a517dd5e93ae
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-03-15 23:25:24 +0530

    RefactorTC1.1RetuenSad

--- test/java/com/Day20/MoodAnalyzerTest.java
+++ test/java/com/Day20/MoodAnalyzerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
  * We have created this test method to check the sad mood.
  * If we provide the string of sad then the method should return sad.
  * We have used the assert equals to check if the method o/p is sad.
+ * @author Tom
  *
  */
 public class MoodAnalyzerTest {
