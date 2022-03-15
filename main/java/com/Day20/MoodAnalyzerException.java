1449dd378fcec1f2ebe27a9da4fb231426a8dd59
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-03-15 23:52:25 +0530

    Custom Exception

---
+++ main/java/com/Day20/MoodAnalyzerException.java
@@ -0,0 +1,14 @@
+package com.Day20;
+
+/**
+ * We have created our custom exception. 
+ * We have used Inheritance and extended the Exception class.
+ * Then we have created a method and passed a message as input.
+ * Inside the method we have called the super. It is a reference variable which is used to refer immediate parent class object.
+ *
+ */
+public class MoodAnalyzerException extends Exception {
+  public MoodAnalyzerException(String message) {
+	  super(message);
+  }
+}
